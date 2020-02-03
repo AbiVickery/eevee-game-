857dab6d25c5a0470c6392b4803510eecd9226a0
Abi Vickery <vickeryabi@gmail.com>
2020-02-03 15:19:05 -0600

    Update buneary.js

---
+++ js/buneary.js
@@ -0,0 +1,19 @@
+function introduction() {
+    print('Buneary!');
+}
+// introduction();
+
+function scene2() {
+    // move sprite
+    // wait
+    // playsound
+    // switch costume
+    // wait
+}
+// scene2();
+
+function lopunny() {
+    // switch costume
+    print('Loppuny!');
+}
+// lopunny();
